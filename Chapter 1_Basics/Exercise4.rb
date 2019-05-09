c3dfcf092c7cfad5021ed6b711b8ccf519ca0874
Lynn Swayze <lynnswayze@gmail.com>
2019-05-09 15:30:18 -0400

    Create Exercise4.rb
    Tested and this ran in https://ruby.github.io/TryRuby/

---
+++ Chapter 1_Basics/Exercise4.rb
@@ -0,0 +1,22 @@
+=begin
+
+ This is from chapter 1, https://launchschool.com/books/ruby/read/basics#exercises
+
+This exercise involves an array with movie titles and the following pre-defined years:
+
+1975
+2004
+2013
+2001
+1981
+
+=end
+
+
+dates = [1975, 2004, 2013, 2001, 1981]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
