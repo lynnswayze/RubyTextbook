b9c78c78e5dd34c612f7d88ceb3d8023b2a70b41
Lynn Swayze <lynnswayze@gmail.com>
2019-05-09 15:06:09 -0400

    Update Exercise1.rb
    Exercise 1 code. Note that I couldn't get this to run, so I need to do some more debugging on it.

--- Chapter 1_Basics/Exercise1.rb
+++ Chapter 1_Basics/Exercise1.rb
@@ -1,1 +1,14 @@
 # This is from chapter 1, https://launchschool.com/books/ruby/read/basics#exercises #
+
+print "What's your first name? "
+First_Name = gets
+First_Name.capitalize!
+print "Got it, #{First_Name}."
+
+print "What's your last name? "
+Last_Name = gets
+Last_Name.capitalize!
+print "Got it, your last name is #{Last_Name}."
+
+
+puts "Your full name is #{First_Name} #{Last_Name}!"
