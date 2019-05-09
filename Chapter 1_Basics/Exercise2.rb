7ac6d5d35308c5697f0c3f42e4878baffcbfb603
Lynn Swayze <lynnswayze@gmail.com>
2019-05-09 15:21:48 -0400

    Exercise Updates

---
+++ Chapter 1_Basics/Exercise2.rb
@@ -0,0 +1,9 @@
+=begin
+
+ This is from chapter 1, https://launchschool.com/books/ruby/read/basics#exercises 
+=end
+
+thousands = 4936 / 1000
+hundreds = 4936 % 1000 / 100
+tens = 4936 % 1000 % 100 / 10
+ones = 4936 % 1000 % 100 % 10
---
+++ Chapter 1_Basics/Exercise3.rb
@@ -0,0 +1,22 @@
+=begin
+
+ This is from chapter 1, https://launchschool.com/books/ruby/read/basics#exercises
+
+This exercise involves an array with movie titles and the following pre-defined years:
+
+1975
+2004
+2013
+2001
+1981
+
+
+=end
+
+{:Jaws => '1975', :Superman2 => '1981', :Shrek => '2001', :Troy => '2004', :Frozen =>'2013'}
+
+[:Jaws]
+[:Superman2]
+[:Shrek]
+[:Troy]
+[:Frozen]
