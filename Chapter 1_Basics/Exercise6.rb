6433db39727c5cbb1179e88b16c12a466069535c
Lynn Swayze <lynnswayze@gmail.com>
2019-05-09 15:42:13 -0400

    Exercise 2.2

---
+++ Chapter 1_Basics/Exercise6.rb
@@ -0,0 +1,13 @@
+=begin
+
+ This is from chapter 1, https://launchschool.com/books/ruby/read/basics#exercises
+
+Instructions:
+"Write a program that calculates the squares of 3 float numbers of your choosing and outputs the result to the screen."
+
+=end
+
+
+puts 4.30 * 4.30
+puts 6.13 * 6.13
+puts 124.34 * 124.34
---
+++ Chapter 2_Variables/Exercise2.1.rb
@@ -0,0 +1,14 @@
+=begin
+
+ This is from chapter 2, https://launchschool.com/books/ruby/read/variables
+
+=end
+
+
+puts "What's your first name? "
+name = gets.chomp
+name.capitalize!
+puts "Got it, #{name}."
+
+
+#Note I cannot get this to run, even after experimenting. Not sure what I'm doing wrong.
