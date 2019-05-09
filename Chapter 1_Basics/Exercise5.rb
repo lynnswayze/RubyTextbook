8b205853c1f3696c191ee6ba408cbc9ac2867a80
Lynn Swayze <lynnswayze@gmail.com>
2019-05-09 15:33:01 -0400

    Create Exercise5.rb
    This runs in https://ruby.github.io/TryRuby/

---
+++ Chapter 1_Basics/Exercise5.rb
@@ -0,0 +1,14 @@
+=begin
+
+ This is from chapter 1, https://launchschool.com/books/ruby/read/basics#exercises
+
+Instructions:
+"Write a program that outputs the factorial of the numbers 5, 6, 7, and 8."
+
+=end
+
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
